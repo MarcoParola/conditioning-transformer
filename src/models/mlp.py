4e2d1bbd9a8760eab8ad50047699faba130ee02d
MarcoParola <marcoparola96@gmail.com>
2024-02-16 10:26:39 +0100

    move MLP transformer network in a separate file: src/models/mlp.py

---
+++ src/models/mlp.py
@@ -0,0 +1,17 @@
+import torch
+from torch import nn, Tensor
+
+
+class MLP(nn.Module):
+    def __init__(self, inputDim: int, hiddenDim: int, outputDim: int, numLayers: int):
+        super().__init__()
+        self.numLayers = numLayers
+
+        h = [hiddenDim] * (numLayers - 1)
+        self.layers = nn.ModuleList(nn.Linear(n, k) for n, k in zip([inputDim] + h, h + [outputDim]))
+
+    def forward(self, x: Tensor) -> Tensor:
+        for i, layer in enumerate(self.layers):
+            x = nn.functional.relu(layer(x)) if i < self.numLayers - 1 else layer(x)
+        return x
+
