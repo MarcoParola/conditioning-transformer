5692e37e470c73153feddf68079904aaa8ab870b
MarcoParola <marcoparola96@gmail.com>
2024-06-18 08:08:47 +0200

    modify script/dataset-stats.py to compute also the distribution of the data over year (per month)

--- scripts/dataset-stats.py
+++ scripts/dataset-stats.py
@@ -1,5 +1,6 @@
 import argparse
 import json
+from matplotlib import pyplot as plt
 
 def compute_stats(json_data):
     category_count = dict()
@@ -12,6 +13,19 @@ def compute_stats(json_data):
     for category in json_data["categories"]:
         num_occurrences = category_count[category["id"]] if category["id"] in category_count else 0
         print("-%s: %d" % (category["name"], num_occurrences))
+
+    # compute date distribution by month
+    date_count = dict()
+    for annotation in json_data["images"]:
+        date = annotation["date_captured"]
+        month = date.split("-")[1]
+        if month not in date_count:
+            date_count[month] = 0
+        date_count[month] += 1
+    # sort and print
+    sorted_dates = sorted(date_count.items(), key=lambda x: x[0])
+    for date, count in sorted_dates:
+        print("%s: %d" % (date, count))
 
 
 if __name__ == '__main__':
